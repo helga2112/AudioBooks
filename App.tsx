b036c6903faee7f0b25ad036eea30bd54e8da861
Olha Dolinher <helga.2112@gmail.com>
2024-01-22 09:49:54 +0200

    navigation added

--- App.tsx
+++ App.tsx
@@ -5,103 +5,30 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import LoginPage  from './src/pages/login/loginPage';
+import { LoginScreen } from './src/screens/login/loginPage';
+import { BooksScreen } from './src/screens/catalog/catalog';
+import { RootNavigationParamList, SCREENS } from './src/navigation/navigations';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-/* function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-} */
+const Stack = createNativeStackNavigator<RootNavigationParamList>();
+
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-     {/*  <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}> */}
-{/*         <Header /> */}
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-            <LoginPage/>
-      
-        </View>
-      {/* </ScrollView> */}
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={SCREENS.Login}>
+        <Stack.Screen name={SCREENS.Login} component={LoginScreen} />
+        <Stack.Screen name={SCREENS.Books} component={BooksScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
-/* const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-}); */
-
 export default App;
+
---
+++ src/screens/basePage/basePageWrapper.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { Colors } from "react-native/Libraries/NewAppScreen"
+
+export const BasePageWrapper = (props: { children?: JSX.Element }): React.JSX.Element => {
+  const {children} = props
+    const isDarkMode = useColorScheme() === 'dark';
+
+    const backgroundStyle = {
+      backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    }
+
+
+    return (
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+       
+          <View
+            style={{
+              backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            }}>
+            
+            {children}
+          </View>
+      </SafeAreaView>
+    );
+  }
+  
